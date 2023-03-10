75d5b50d25e4e25739e0029fa0e9437e30ae6fbe
mmardiansyah17 <mardiansyahm002@gmail.com>
2023-03-10 22:22:31 +0700

    perbaikan

--- src/components/ConversationSection.jsx
+++ src/components/ConversationSection.jsx
@@ -10,16 +10,27 @@ export default function ConversationSection({ chatSelected }) {
     <div className=" basis-[30%] bg-[#111B21]  border-r border-[#1d2429]">
       <HeaderConversation />
       <SearchConversation />
-      <div className="flex items-center space-x-3 mt-4 text-lg px-4">
-        <FontAwesomeIcon icon={faArchive} className="text-[#038E72]" />
-        <h4 className=" w-full border-b border-slate-600">Diarsipkan</h4>
-      </div>
-      <div className="mt-5">
-        <ChatItem chatSelected={chatSelected} />
-        <ChatItem chatSelected={chatSelected} />
-        <ChatItem chatSelected={chatSelected} />
-        <ChatItem chatSelected={chatSelected} />
-        <ChatItem chatSelected={chatSelected} />
+      <div className=" overflow-y-auto h-[80%]">
+        <div className="flex items-center space-x-3 mt-4 text-lg px-4">
+          <FontAwesomeIcon icon={faArchive} className="text-[#038E72]" />
+          <h4 className=" w-full border-b border-slate-600">Diarsipkan</h4>
+        </div>
+        <div className="mt-5">
+          <ChatItem chatSelected={chatSelected} />
+          <ChatItem chatSelected={chatSelected} />
+          <ChatItem chatSelected={chatSelected} />
+          <ChatItem chatSelected={chatSelected} />
+          <ChatItem chatSelected={chatSelected} />
+          <ChatItem chatSelected={chatSelected} />
+          <ChatItem chatSelected={chatSelected} />
+          <ChatItem chatSelected={chatSelected} />
+          <ChatItem chatSelected={chatSelected} />
+          <ChatItem chatSelected={chatSelected} />
+          <ChatItem chatSelected={chatSelected} />
+          <ChatItem chatSelected={chatSelected} />
+          <ChatItem chatSelected={chatSelected} />
+          <ChatItem chatSelected={chatSelected} />
+        </div>
       </div>
     </div>
   );
