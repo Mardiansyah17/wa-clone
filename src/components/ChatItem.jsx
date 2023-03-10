2a6eed47a68acaedc82891527986550ecf382305
mmardiansyah17 <mardiansyahm002@gmail.com>
2023-03-10 21:57:37 +0700

    perbaikan

--- src/components/ChatItem.jsx
+++ src/components/ChatItem.jsx
@@ -2,14 +2,14 @@ import { faCheckDouble } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import CheckIcon from "./icon/CheckIcon";
-
+import mark from "/public/mark.jpg";
 export default function ChatItem({ chatSelected }) {
   return (
     <div
       onClick={chatSelected}
       className="flex cursor-pointer items-center mb-4  space-x-3 text-[#E9ECEC] px-3"
     >
-      <img src="/public/mark.jpg" className="w-10 rounded-full h-10 object-fill" alt="" />
+      <img src={mark} className="w-10 rounded-full h-10 object-fill" alt="" />
       <div className="border-b border-slate-600 pb-2 w-full ">
         <div className="flex items-center justify-between">
           <span className="font-semibold">Bedul</span>
